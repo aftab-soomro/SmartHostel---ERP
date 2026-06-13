c76379f6aa4dad467fca65930df302390b171824
Aftab <aftabgaming571@gmail.com>
2026-06-13 13:38:36 +0500

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'https://thunderous-cuchufli-a2a6ea.netlify.app'],
   credentials: true,
 }));
 app.use(express.json());
