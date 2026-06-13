679a47b3a94634487c144145fec10442b8470249
Aftab <aftabgaming571@gmail.com>
2026-06-13 13:59:01 +0500

    Update CORS for new URL

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'https://thunderous-cuchufli-a2a6ea.netlify.app'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'https://smarthostel-erp.netlify.app'],
   credentials: true,
 }));
 app.use(express.json());
